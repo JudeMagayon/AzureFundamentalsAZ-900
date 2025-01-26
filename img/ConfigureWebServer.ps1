f36eff6bb799244279aff57b36148231f6e57491
JudeMagayon <judecmagayon@gmail.com>
2025-01-26 13:00:24 +0800

    Update ConfigureWebServer.ps1

--- img/ConfigureWebServer.ps1
+++ img/ConfigureWebServer.ps1
@@ -14,7 +14,7 @@ Configuration DeployWebPage
             DestinationPath = "C:\inetpub\wwwroot\index.html"
             Force = $true
             Type = "File"
-            Contents = "<html><body><img src='https://github.com/JudeMagayon/AzureFundamentalsAZ-900/blob/main/img/HeyJude.png'/><h1>Hey Looks like my AZ-900 training is going great!</h1></body></html>"
+            Contents = "<html><body><img src='https://github.com/JudeMagayon/AzureFundamentalsAZ-900/blob/fd44d17267ce7d8ad3266352504cc438de4e8c3a/img/HeyJude.png'/><h1>Hey Looks like my AZ-900 training is going great!</h1></body></html>"
         }
     }
 }
